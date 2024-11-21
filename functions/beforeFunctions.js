cb5520c40fcf86451bfeba1f1ab3efd4de2ada50
ricardolzp <ricardoluiz_p@hotmail.com>
2024-11-21 14:04:26 -0300

    ajuste

--- functions/beforeFunctions.js
+++ functions/beforeFunctions.js
@@ -10,16 +10,16 @@ const { WELCOME_TEXT } = require("./constants")
 BeforeCreateUser = async (req) => {
     const { email, username } = req.data;
 
-    const emailExists = await  SELECT.one.from(Users).where({email})
-    if (emailExists) return req.error(409, 'EMAIL_ALREADY_EXISTS', [email]);
+    // const emailExists = await  SELECT.one.from(Users).where({email})
+    // if (emailExists) return req.error(409, 'EMAIL_ALREADY_EXISTS', [email]);
 
-    const usernameExists = await SELECT.one.from(Users).where({ username });
-    if (usernameExists) return req.error(409, 'USERNAME_ALREADY_EXISTS', [username]);
+    // const usernameExists = await SELECT.one.from(Users).where({ username });
+    // if (usernameExists) return req.error(409, 'USERNAME_ALREADY_EXISTS', [username]);
     
-    const { password } = req.data;
-    if (password) req.data.password = await PasswordEncryptor.encryptPassword(password);
+    // const { password } = req.data;
+    // if (password) req.data.password = await PasswordEncryptor.encryptPassword(password);
 
-    sendMail(email, WELCOME_TEXT)
+    // sendMail(email, WELCOME_TEXT)
 }
 
 module.exports = { BeforeCreateUser }
